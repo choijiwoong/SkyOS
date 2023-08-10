df88fb6278bb57e1ef95994da96f2b2ea788acea
최지웅 <gogogi313@naver.com>
2023-08-10 11:20:22 +0900

    이미 메모리에 있는 코드를 엔트리 포인트로 설정할 경우 이미 페이지에 매핑되어있지만, 파일에서 로드할 때는 동적으로 페이지 매핑이 필요하다. 파일정보를 가져오기 위해 커널스레즈페이지디렉토리에 ImageBase를 매핑하고, 현재프로세스메모리에도 매핑시킨다. 데이터를 읽은 후 스택을 생성하고 esp, ebp를 스레드에 설정하고, 파일 로드에 사용한 커널 태스르의 페이지 테이블을 UnmapPageTable로 회수한다

--- 13장. 커널 코어 구현/ProcessManager.cpp
+++ 13장. 커널 코어 구현/ProcessManager.cpp
@@ -36,33 +36,6 @@ class Thread{
 		DWORD m_threadId;
 };
 
-Thread* ProcessManager::CreateThread(Process* pProcess, LPTHREAD_START_ROUTINE lpStartAddress, LPVOID param){
-	Thread* pThread=new Thread();
-	pThread->m_pParent=pProcess;
-	pThread->SetThreadId(m_nextThreadId++);
-	pThread->m_dwPriority=1;
-	pThread->m_taskState=TASK_STATE_INIT;
-	pThread->m_waitingTime=TASK_RUNNING_TIME;
-	pThread->m_stackLimit=STACK_SIZE;
-	pThread->m_imageBase=0;
-	pThread->m_imageSize=0;
-	memset(&pThread->frame, 0, sizeof(trapFrame));
-	pThread->frame.eip=(uint32_t)lpStartAddress;//EIP_스레드의 엔트리를 나타내는 함수 주소. 
-	pThread->frame.flags=0x200;//플래그 
-	pThread->m_startParam=param;//파라미터_EIP에 있는 시작주소를 실행하며 사용할 플래그와 매개변수 지정인가? 위엔 메타정보이고. 실제 수행할 작업의 정보는 trapFrame타입에 저장하나보네 
-	
-	//stack
-	void* stackAddress=(void*)(g_stackPhysicalAddressPool - STACK_SIZE * kernelStackIndex++);//사용할 스택의 크기만큼 스택의 시작주소__가용한 스택물리메모리를 스택 크기만큼 스레드마다 할당하기 위함인듯 
-	
-	//ESP, EBP가 스택을 가리킨다. 
-	pThread->m_initialStack=(void*)((uint32_t)stackAddress+STACK_SIZE);
-	pThread->frame.esp=(uint32_t)pThread->m_initialStack;
-	pThread->frame.ebp=pThread->frame.esp;
-	
-	m_taskList->push_back(pThread);//태스크리스트에 스레드 추가 
-	return pThread;
-}
-
 class ProcessManager{
 	public:
 		.....
@@ -121,3 +94,123 @@ Process* ProcessManager::CreateProcessFromMemory(const char* appName, LPTHREAD_S
 	result=AddProcess(pProcess);//현재 프로세스를 프로세스 리스트에 추가 
 	return pProcess; //현재 프로세스를 반환 
 }
+
+Thread* ProcessManager::CreateThread(Process* pProcess, LPTHREAD_START_ROUTINE lpStartAddress, LPVOID param){//메모리모부터 생성 
+	Thread* pThread=new Thread();
+	pThread->m_pParent=pProcess;
+	pThread->SetThreadId(m_nextThreadId++);
+	pThread->m_dwPriority=1;
+	pThread->m_taskState=TASK_STATE_INIT;
+	pThread->m_waitingTime=TASK_RUNNING_TIME;
+	pThread->m_stackLimit=STACK_SIZE;
+	pThread->m_imageBase=0;
+	pThread->m_imageSize=0;
+	memset(&pThread->frame, 0, sizeof(trapFrame));
+	pThread->frame.eip=(uint32_t)lpStartAddress;//EIP_스레드의 엔트리를 나타내는 함수 주소. 
+	pThread->frame.flags=0x200;//플래그 
+	pThread->m_startParam=param;//파라미터_EIP에 있는 시작주소를 실행하며 사용할 플래그와 매개변수 지정인가? 위엔 메타정보이고. 실제 수행할 작업의 정보는 trapFrame타입에 저장하나보네 
+	
+	//stack
+	void* stackAddress=(void*)(g_stackPhysicalAddressPool - STACK_SIZE * kernelStackIndex++);//사용할 스택의 크기만큼 스택의 시작주소__가용한 스택물리메모리를 스택 크기만큼 스레드마다 할당하기 위함인듯 
+	
+	//ESP, EBP가 스택을 가리킨다. 
+	pThread->m_initialStack=(void*)((uint32_t)stackAddress+STACK_SIZE);
+	pThread->frame.esp=(uint32_t)pThread->m_initialStack;
+	pThread->frame.ebp=pThread->frame.esp;
+	
+	m_taskList->push_back(pThread);//태스크리스트에 스레드 추가 
+	return pThread;
+}
+
+Thread* ProcessManager::CreateThread(Process* pProcess, FILE* file, LPVOID param){
+	unsigned char buf[512];
+	IMAGE_DOS_HEADER* dosHeader=0;
+	IMAGE_NT_HEADERS* ntHeaders=0;
+	unsigned char* memory=0;
+	
+	int readCnt=StorageManager::GetInstance()->ReadFile(file, buf, 1, 512);//버퍼를 이용하여 file을 읽는다 
+	if(readCnt==0)
+		return nullptr;
+	
+	if(!ValidatePEImage(buf)){//유효한 PE파일인지_실행하려는 응용프로그램의 이미지 베이스 검증
+		SkyConsole::Print("Invalid PE Format!! %s\n", pProcess->m_processName);
+		StorageManager::GetInstance()->CloseFile(file);
+		return nullptr;
+	}
+	
+	dosHeader=(IMAGE_DOS_HEADER*)buf;//PE이미지의 헤더를 계산, OptionalHeader의 이미지 베이스와 이미지의 크기를 계산한다?
+	ntHeaders=(IMAGE_NT_HEADERS*)(dosHeader->e_lfanew+(uint32_t)buf);
+	
+	pProcess->m_imageBase=ntHeaders->OptionalHeader.ImageBase;//위의 nt헤더에 계산된 이미지 베이스와 이미지 크기를 프로세스에 저장. 
+	pProcess->m_ImageSize=ntHeaders->OptionalHeader.SizeOfImage;
+	
+	Thread* pThread=new Thread();//새 스레드 생성 
+	.....//스레드에 응용프로그램 정보 할당 
+	
+	int pageRest=0;//파일로부터 읽을 데이터 페이지 수 계산 
+	if((pThread->m_imageSize%4096)>0)//4096단위로 읽기가 가능하다면 
+		pageRest=1;
+	pProcess->m_dwPageCount=(pThread->m_imageSize/4096)+pageRest;//스레드의 이미지 사이즈 갱신 
+	
+	//물리주소를 가상주소로 매핑 
+	unsigned char* physicalMemory=(unsigned char*)PhysicalMemoryManager::AllocBlocks(pProcess->m_dwPageCount);
+	
+	//1. 커널 스레드 페이지 디렉토리에 가상주소 매핑 
+	for(DWORD i=0; i<pProcess->m_dwPageCount; i++){
+		VirtualMemoryManager::MapPhysicalAddressToVirtualAddress(
+			GetCurrentTask()->m_pParent->GetPageDirecgtory(),//현재 작업중인 태스크의 부모디렉토리(커널 스레드 페이지 디렉토리)에 
+			ntHeaders->OptionalHeader.ImageBase+i*PAGE_SIZE,//이미지 베이스에서 4096만큼을 
+			(uint32_t)physicalMemory+i*PAGE_SIZE,//물리메모리에 매핑 
+			I86_PTE_PRESENT | I86_PTE_WRITABLE//쓰기가능하고 PTE이미지이다 
+		);
+	}
+	
+	//생성될 스레드의 페이지 디렉토리에 파일 이미지 매핑 
+	for(DWORD i=0; i<pProcess->m_dwPageCount; i++){
+		VirtualMemoryManager::MapPhysicalAddressToVirtualAddress(
+			pProcess->GetPageDirectory(),//현재 프로세스에서 진행중인 디렉토리에(인자로 들어옴. 스레드가 생성될 현재 디렉토리) 
+			ntHeaders->OptionalHeader.ImageBase+i*PAGE_SIZE,
+			(uint32_t)physicalMemory+i*PAGE_SIZE,
+			I86_PTE_PRESENT | I86_PTE_WRITABLE
+		);
+	}
+	
+	memory=(unsigned char*)ntHeaders->OptionalHeader.ImageBase;//이미지 베이스 
+	memset(memory, 0, pThread->m_imageSize);//이미지 크기만큼을 0으로 초기화 
+	memcpy(memory, buf, 512);//buf의 내용을 memory의 내용을 메모리에 복사 
+	
+	//파일을 메모리에 로드_위와 같은 방식으로 512단위 
+	int fileRest=0;
+	if((pThread->m_imageSize%512)!=0)
+		fileRest=1;
+	
+	int readCount=(pThread->m_imageSize/512)+fileRest;
+	for(int i=1; i<readCount; i++){
+		if(file->_eof==1)
+			break;
+		
+		readCount=StorageManager::GetInstance()->ReadFile(file, memory+512*i, 512, 1);//파일을 메모리에 512씩 읽는다 
+	}
+	
+	//스택생성, 주소공간 매핑
+	void* stackAddress=(void*)(g_stackPhysicalAddressPool-PAGE_SIZE*10*kernelStackIndex++);//페이지사이즈를 통해 가용한 스택 주소를 얻는다
+	
+	//스레드에 ESP, EBP설정, 스택포인터 베이스포인터
+	pThread->m_initialStack=(void*)((uint32_t)stackAddress+PAGE_SIZE*10);//스레드에서 사용할 스택정보들 
+	pThread->frame.esp=(uint32_t)pThread->m_initialStack;
+	pThread->frame.ebp=pThread->frame.esp;
+	.....
+	
+	//파일로드에 사용한 커널태스크 페이지테이블 회수
+	for(DWORD i=0; i<pProcess->m_dwPageCount; i++){
+		VirtualMemoryManager::UnmapPageTable(//메모리에 있는 데이터를 읽은 것이 아니라, PE이미지를 읽은 것이기에 따로 페이지 매핑을 진행한 것이고 이를 회수.PE파일에 대한 이해는 부록을 참고해 따로 보쟈.. 
+			GetCurrentTask()->m_pParent->GetPageDirectory(),//프로세스 페이지는 앞으로 쓸 것이기에 냅두지만, PE이미지는 커널에서 접근해서 가져온 것이기에 이만 회수하는 것 같다. 
+			(uint32_t)physicalMemory+i*PAGE_SIZE
+		);
+	}
+	
+	//태스크 리스트 추가 
+	m_taskList->push_back(pThread);
+	
+	return pThread;
+} 
